08defaf3e649d09843b7ca355439c1fc984c166b
indominousrexx <65479911+indominousrexx@users.noreply.github.com>
2020-05-22 21:26:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,103 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
 
-startGame()
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let miliseconds = 0;
 
-restartButton.addEventListener('click', startGame)
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+let displayMiliSeconds = 0;
 
-function startGame() {
-  circleTurn = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
-}
+let interval = null;
 
-function handleClick(e) {
-  const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  placeMark(cell, currentClass)
-  if (checkWin(currentClass)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
-  } else {
-    swapTurns()
-    setBoardHoverClass()
+let status = "stopped";
+
+function stopWatch(){
+
+    miliseconds++;
+     if(miliseconds / 100 === 1){
+       miliseconds = 0;
+       seconds++;
+         if(seconds / 60 === 1){
+            seconds = 0;
+            minutes++;
+ 
+            if(minutes / 60 === 1){
+               minutes = 0;
+               hours++;
+           }
+        }
+     }
+
+    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
+    if(miliseconds < 100 && miliseconds >= 10) {
+      displayMiliSeconds = "" + miliseconds.toString();
   }
-}
-
-function endGame(draw) {
-  if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
-  } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  else{
+    if(miliseconds < 10) 
+    {
+      displayMiliSeconds = "0" + miliseconds.toString();
+  } else
+      displayMiliSeconds = miliseconds;
   }
-  winningMessageElement.classList.add('show')
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
+    }
+
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
+    }
+
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
+    }
+
+    //Display updated time values to user
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds + ":" + displayMiliSeconds;
+
 }
 
-function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  })
+
+
+function startStop(){
+
+    if(status === "stopped"){
+
+        //Start the stopwatch (by calling the setInterval() function)
+        interval = window.setInterval(stopWatch, 10);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+
+    }
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+
+    }
+
 }
 
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
-}
+//Function to reset the stopwatch
+function reset(){
 
-function swapTurns() {
-  circleTurn = !circleTurn
-}
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    miliseconds = 0;
+    document.getElementById("display").innerHTML = "00:00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
 
-function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
-  if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
-  } else {
-    board.classList.add(X_CLASS)
-  }
-}
-
-function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
 }
